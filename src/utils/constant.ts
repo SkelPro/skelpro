45f48312c85820245de077ea90c89c1c5c16b5f4
Sydney205 <diekechi@gmail.com>
2025-07-08 23:25:55 +0100

    New Version [1.4.0]

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,3 +1,3 @@
-export const VERSION = "1.3.4";
+export const VERSION = "1.4.0";
 export const packageName = "skelpro";
 export const newsUrl = "https://raw.githubusercontent.com/SkelPro/new-updates/refs/heads/main/new-updates.json";
