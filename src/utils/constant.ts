dd9456bf49940b91f42645d271929759fd2a1c44
Sydney205 <diekechi@gmail.com>
2025-07-11 19:13:36 +0100

    refactor: Capitalise constants and refactor codes

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,4 +1,13 @@
 export const VERSION = "1.4.0";
-export const packageName = "skelpro";
-export const newsUrl =
+export const PACKAGE_NAME = "skelpro";
+export const NEWS_URL =
   "https://raw.githubusercontent.com/SkelPro/new-updates/refs/heads/main/new-updates.json";
+
+export const ASCII_ART = `
+\n  ███████╗ ██╗  ██╗ ███████╗ ██╗      ██████╗  ██████╗   ██████╗ 
+  ██╔════╝ ██║ ██╔╝ ██╔════╝ ██║      ██╔══██╗ ██╔══██╗ ██╔═══██╗
+  ███████╗ █████╔╝  █████╗   ██║      ██████╔╝ ██████═╝ ██║   ██║
+        ██║ ██╔═██╗  ██╔══╝   ██║      ██╔═══╝  ██ ██╗   ██║   ██║
+  ███████║ ██║  ██╗ ███████╗ ███████╗ ██║      ██║ ██╗  ╚██████╔╝
+  ╚══════╝ ╚═╝  ╚═╝ ╚══════╝ ╚══════╝ ╚═╝      ╚═╝ ╚═╝   ╚═════╝  ${VERSION}
+`;
